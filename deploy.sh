410b9e857524ac55b6a847c962414a5733661f31
Simon <simobilleter@gmail.com>
2024-04-19 18:11:39 +0200

    remove unneeded qemu-user-static

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,6 @@ function sync_docker() {
     printf "\ncreate new version:\n"
     read -r VERSION
 
-    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
     docker buildx build \
         --platform linux/amd64,linux/arm64 \
         -t bbilly1/redditbot \
